bcbfb31fad228fc92f73b993c2af521fba0cc81f
ha <fake@example.com>
2019-07-15 12:53:05 +0900

    update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/notesquiz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bfeaghqetldexo:22bac7c1792bdfc1baf5c8b87b662e53ff2289b54c84fd545c0a0939be57eb65@ec2-54-227-251-33.compute-1.amazonaws.com:5432/dbctmkc2ofi91l'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
+heroku = Heroku(app)
 # Create our database model
 class User(db.Model):
     __tablename__ = "users"
