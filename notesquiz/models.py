c2a6f59751a2a0b32cc05c1504a94ba78e40e56d
ha <fake@example.com>
2019-07-16 21:14:24 +0900

    full-featured app

---
+++ notesquiz/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from notesquiz import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    notes = db.relationship('Note', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User ('{self.username}')"
+
+class Note(db.Model):
+    __tablename__ = "note"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Note ('{self.title}', '{self.date_posted}')"
